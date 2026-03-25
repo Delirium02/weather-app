b3f08d02196c2de4a0d49a4ede6e59a2422e944e
delirium02 <darknightbs@gmail.com>
2026-03-25 20:26:23 +1100

    Added background, and implemented the input search layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 async function fetchWeatherData(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=RX4APM5AGNAJYPE3LWW7AT5KD`);
     const weatherData = await response.json();
@@ -6,13 +8,21 @@ async function fetchWeatherData(location) {
     return weatherData;
 }
 
+function toCelsius(f) {
+    return (f - 32) * 5/9;
+}
+
+function toFahrenheit(c) {
+    return (c * 9/5) + 32;
+}
 
 function processData(weatherData) {
     const currentConditions = weatherData.currentConditions;
     const dailyForecasts = weatherData.days;
     const temp = currentConditions.temp;
-
-    console.log(`Current temperature: ${temp}°C`);
 }
 
-fetchWeatherData("melbourne").then(processData);
+
+
+
+// fetchWeatherData("melbourne").then(processData);
