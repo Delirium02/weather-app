1737177f24fa49fde98943a33288ee55627a4db2
delirium02 <darknightbs@gmail.com>
2026-03-29 02:20:05 +1100

    Implemented dynamic importing for weather icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./style.css";
 const getWeatherBtn = document.querySelector(".get-weather-btn");
 const weatherInfo = document.querySelector(".weather-info");
 
+
+// Initially hide weather info section
+weatherInfo.classList.remove("weather-info");
+
 async function fetchWeatherData(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=RX4APM5AGNAJYPE3LWW7AT5KD`);
     const weatherData = await response.json();
@@ -25,25 +29,40 @@ function processData(weatherData) {
     const temp = currentConditions.temp;
 }
 
-// Get Weather Button Event Listener
+async function getIcon(iconName) {
+    try {
+        const image = await import(`./images/weather-icons/${iconName}.svg`);
+        return image.default;
+    } catch {
+        console.log(`Icon not found: ${iconName}`);
+        return null;
+    }
+}
 
+// Get Weather Button Event Listener
 getWeatherBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    const locationInput = document.querySelector(".location-input").value;
+
+    const locationInput = document.querySelector(".location-input").value;   
     const rawData = await fetchWeatherData(locationInput);
     processData(rawData);
 
     const locationAddress = rawData.address;
+    const conditions = rawData.currentConditions.conditions;
     const currentTempC = toCelsius(rawData.currentConditions.temp);
+
+    const iconName = rawData.currentConditions.icon;
+    const iconUrl = await getIcon(iconName);
     
 
     weatherInfo.innerHTML = `<h2>${locationAddress.charAt(0).toUpperCase() + locationAddress.slice(1)}</h2>
-<p>Current Temperature: ${currentTempC}°C</p>
-<p>Conditions: ${rawData.currentConditions.conditions}</p>
-`;
+                            <div class="weather-conditions">
+                                ${iconUrl ? `<img src="${iconUrl}" alt="${iconName}" class="weather-icon">` : ""}
+                                <p>Conditions: ${conditions}</p>
+                            </div>
+                            <p class="temperature-display">Current Temperature: ${currentTempC.toFixed(0)}°C</p>`;
 
+
+    weatherInfo.classList.add("weather-info");
     weatherInfo.classList.remove("display-none");
-})
-
-
-// fetchWeatherData("melbourne").then(processData);
+});
